83b9efa31764357f32c5d72d44b1fdd92d1286f3
autovis <autovisgit@gmail.com>
2015-05-02 11:49:56 +0000

    Oanda: Use 'request' module for api reqs; improved error reporting (part II)

--- datasources/oanda.js
+++ datasources/oanda.js
@@ -1,5 +1,4 @@
-var http = require('http');
-var https = require('https');
+var request = require('request');
 var requirejs = require('requirejs');
 
 var async = requirejs('async');
@@ -16,8 +15,10 @@ var default_config = {
     auth_token: process.env.OANDA_ACCESS_TOKEN
 }
 
-//var oanda_server = 'fxpractice.oanda.com';
-var oanda_server = 'sandbox.oanda.com';
+//var api_server = 'https://api-fxpractice.oanda.com';
+//var stream_server = 'https://stream-fxpractice.oanda.com';
+var api_server = 'http://api-sandbox.oanda.com';
+var stream_server = 'http://stream-sandbox.oanda.com';
 
 var instrument_mapping = {
     "audcad": "AUD_CAD",
@@ -32,95 +33,88 @@ var instrument_mapping = {
 }
 
 function subscribe(socket, params, options) {
-    if (!_.has(instrument_mapping, params[0])) throw new Error("Instrument '"+params[0]+"' not mapped to OANDA equivalent identifier");
+    if (!_.has(instrument_mapping, params[0])) throw new Error("Instrument '" + params[0] + "' not mapped to OANDA equivalent identifier");
     var instrument = instrument_mapping[params[0]];
     var datasource = ["oanda"].concat(params).join(":");
     var config = _.defaults(options, default_config);
-    var payload = "";
+
     async.series([
 
-        function(cb) { // historical candles
-            var https_options = {
+        // fetch historical candles
+        function(cb) {
+            var http_options = {
                 method: 'GET',
-                host: 'api-' + oanda_server,
-                path: '/v1/candles?candleFormat=bidask&granularity='+(params[1] || config.timeframe).toUpperCase()+'&count='+config.history+'&instrument='+instrument,
-                headers: {"Authorization" : "Bearer "+config.auth_token},
+                url: api_server + '/v1/candles?candleFormat=bidask&granularity=' + (params[1] || config.timeframe).toUpperCase() + '&count=' + config.history + '&instrument=' + instrument,
+                headers: {"Authorization": "Bearer " + config.auth_token},
             };
-            var request = https.request(https_options, function(response) {
-                response.on("data", function(chunk) {
-                    payload += chunk;
-                });
-                response.on("end", function() {
-                    var response = JSON.parse(payload);
-                    if (response.code) {
-                        socket.emit("server_error", response);
-                        cb(response);
+            var payload = '';
+            var hist_req = request(http_options);
+            hist_req.on('data', function(chunk) {
+                payload += chunk.toString();
+            });
+            hist_req.on('end', function() {
+                try {
+                    var resp = JSON.parse(payload);
+                } catch (e) {
+                    return cb(e);
+                }
+                _.each(resp.candles, function(candle) {
+                    var bar = {
+                        date: date2string(new Date(candle.time)),
+                        ask: {
+                            open: candle.openAsk,
+                            high: candle.highAsk,
+                            low: candle.lowAsk,
+                            close: candle.closeAsk
+                        },
+                        bid: {
+                            open: candle.openBid,
+                            high: candle.highBid,
+                            low: candle.lowBid,
+                            close: candle.closeBid
+                        },
+                        volume: candle.volume
                     };
-                    _.each(response.candles, function(candle) {
-                        var bar = {
-                            date: date2string(new Date(candle.time)),
-                            ask: {
-                                open: candle.openAsk,
-                                high: candle.highAsk,
-                                low: candle.lowAsk,
-                                close: candle.closeAsk
-                            },
-                            bid: {
-                                open: candle.openBid,
-                                high: candle.highBid,
-                                low: candle.lowBid,
-                                close: candle.closeBid
-                            },
-                            volume: candle.volume
-                        };
-                        socket.emit("data", {datasource: datasource, data: bar, type: "candle"});
-                    })
-                    cb();
-                });
+                    socket.emit('data', {datasource: datasource, data: bar, type: 'candle'});
+                })
+                cb();
             });
-            request.on('error', function(err) {
-                socket.emit("server_error", err);
-                cb(err);
-            });
-            request.end();
+            hist_req.on('error', cb);
+            hist_req.end();
         },
 
-        function(cb) { // real-time tick streaming
-            var https_options = {
+        // real-time rates streaming
+        function(cb) {
+            var http_options = {
                 method: 'GET',
-                host: 'stream-' + oanda_server,
-                path: '/v1/prices?accountId='+config.account_id.toString()+'&instruments='+instrument,
-                headers: {"Authorization" : "Bearer "+config.auth_token},
+                url: stream_server + '/v1/prices?accountId=' + config.account_id.toString() + '&instruments=' + instrument,
+                headers: {'Authorization': 'Bearer ' + config.auth_token},
             };
-            var request = http.request(https_options, function(response) {
-                var packet;
-                response.on("data", function(chunk) {
-                    var match, packet;
-                    var rest = chunk.toString();
-                    // Break apart multiple JSON objects bunched together in same response chunk
-                    while (match = rest.match(/^\s*({(?:[^{}]|{[^{}]*})*})\s*(.*)\s*$/)) {
-                        packet = JSON.parse(match[1]);
-                        if (_.has(packet, "tick")) {
-                            var tick = {date: date2string(new Date(packet.tick.time)), ask: packet.tick.ask, bid: packet.tick.bid};
-                            socket.emit("data", {datasource: datasource, data: tick, type: "tick"});
-                        }
-                        rest = match[2];
+            var rates_req = request(http_options);
+            rates_req.on("data", function(chunk) {
+                var match, packet;
+                var rest = chunk.toString();
+                // Break apart multiple JSON objects bunched together in same response chunk
+                while (match = rest.match(/^\s*({(?:[^{}]|{[^{}]*})*})\s*(.*)\s*$/)) {
+                    packet = JSON.parse(match[1]);
+                    if (_.has(packet, "tick")) {
+                        var tick = {date: date2string(new Date(packet.tick.time)), ask: packet.tick.ask, bid: packet.tick.bid};
+                        socket.emit("data", {datasource: datasource, data: tick, type: "tick"});
                     }
-                    cb();
-                });
-                response.on("error", function(err) {
-                    socket.emit("server_error", err);
-                    cb(err);
-                });
-                response.on("end", function() {
-                    socket.emit("end", datasource);
-                });
+                    rest = match[2];
+                }
+                cb();
             });
-            request.end();
+            rates_req.on("error", cb);
+            rates_req.on("end", function() {
+                socket.emit("end", datasource);
+            });
+            rates_req.end();
         }
     ], function(err) {
         if (err) {
-            console.error(err);
+            console.error(new Date(), 'ERROR in oanda.js: ', err);
+            socket.emit('server_error', err);
         }
     });
 
