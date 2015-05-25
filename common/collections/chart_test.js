1986bf95bd0d8ea6e8ed324c5533925d9f204adf
autovis <autovisgit@gmail.com>
2015-05-25 18:15:02 +0000

    Initial design for defining collections with object constructors

--- common/collections/chart_test.js
+++ common/collections/chart_test.js
@@ -1,25 +1,47 @@
-define({
+'use strict';
 
-    // Input streams
-    "ask":                   ["0",                              "stream:AskBar"],
-    "bid":                   ["0",                              "stream:BidBar"],
-    "m30":                   [{tf:"m30"}, "ask",                "tf:Candle2Candle"],
+define(['underscore', 'objects/collection'], function(_, col) {
 
-    // Indicator definitions
-    "test":                   ["ask.close",                      "test:EmbeddedIndicator", 10],
-    "atr":                    ["ask",                            "ATR", 9],
-    "sdl_s":                  ["ask.close",                      "SDL", 78],
-    "sdl_f":                  ["ask.close",                      "SDL", 10],
-    "srsi_m":                 ["ask.close",                      "StochRSI", 14, 14, 5, 3],
-    "srsi_f":                 ["ask.close",                      "StochRSI", 3, 3, 3, 2],
-    "kvo":                    ["ask",                            "KVO", 34, 55, 21],
-    "kvo_sdl":                ["kvo.KO",                         "SDL", 13],
-    "obv":                    ["ask",                            "OBV"],
-    "ema":                    ["ask.close",                      "EMA", 10],
-    "m30ema":                 ["m30.close",                      "EMA", 10],
-    "m30obv":                 ["m30",                            "OBV"],
-    "m30sdl":                 ["m30.close",                      "SDL", 10],
-    "obv_t":                  ["obv",                            "SDL", 13],
-    "obv_sdl":                ["obv",                            "SDL", 55]
+    var Indicator = col.Indicator;
+    var Collection = col.Collection;
+    var Config = col.Config;
 
-})
+    return {
+
+        // Define collection input sources
+
+        inputs: [
+            {id: 2, type: 'dual_candle_bar', tf: '@htf', ds: '@source:@instrument:@htf:@chart.maxsize'},
+            {id: 1, type: 'dual_candle_bar', tf: '@ltf', ds: '@source:@instrument:@ltf:@chart.maxsize'},
+            {id: 0, type: 'tick', tf: 'T', ds: '@source:@instrument', subscribe: true}
+        ],
+
+        network: {
+
+            // Input streams
+            'ask':                   [0,                Indicator('stream:AskBar')],
+            'bid':                   [0,                Indicator('stream:BidBar')],
+            'm30':                   ['ask',            Indicator('tf:Candle2Candle'),                  Config({tf:'m30'})],
+
+            // Indicator definitions
+            'test':                   ['ask.close',     Indicator('test:EmbeddedIndicator', 10)],
+            'atr':                    ['ask',           Indicator('ATR', 9)],
+            'sdl_s':                  ['ask.close',     Indicator('SDL', 78)],
+            'sdl_f':                  ['ask.close',     Indicator('SDL', 10)],
+            'srsi_m':                 ['ask.close',     Indicator('StochRSI', 14, 14, 5, 3)],
+            'srsi_f':                 ['ask.close',     Indicator('StochRSI', 3, 3, 3, 2)],
+            'kvo':                    ['ask',           Indicator('KVO', 34, 55, 21)],
+            'kvo_sdl':                ['kvo.KO',        Indicator('SDL', 13)],
+            'obv':                    ['ask',           Indicator('OBV')],
+            'ema':                    ['ask.close',     Indicator('EMA', 10)],
+            'm30ema':                 ['m30.close',     Indicator('EMA', 10)],
+            'm30obv':                 ['m30',           Indicator('OBV')],
+            'm30sdl':                 ['m30.close',     Indicator('SDL', 10)],
+            'obv_t':                  ['obv',           Indicator('SDL', 13)],
+            'obv_sdl':                ['obv',           Indicator('SDL', 55)]
+
+        }
+
+    };
+
+});
