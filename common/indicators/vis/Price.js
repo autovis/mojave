f955e6901fd63a93da5ebbca5fade8f69b1616b4
autovis <autovis@users.noreply.github.com>
2015-10-23 12:04:55 -0400

    Add and apply 'wickoffset' option to vis:Price; ESLint fixes

--- common/indicators/vis/Price.js
+++ common/indicators/vis/Price.js
@@ -22,50 +22,48 @@ define([], function() {
 
         vis_render: function(d3, vis, options, cont) {
 
-            this.wicks = cont.append("g").attr("class", "wicks");
-            this.candles = cont.append("g").attr("class", "candles");
+            this.wicks = cont.append('g').attr('class', 'wicks');
+            this.candles = cont.append('g').attr('class', 'candles');
 
             options._indicator.indicator.vis_update.apply(this, [d3, vis, options]);
         },
 
         vis_update: function(d3, vis, options) {
 
-            if (_.isArray(options.dasharray)) throw new Error("asdf");
-
-            var wick = this.wicks.selectAll("line.wick")
+            var wick = this.wicks.selectAll('line.wick')
               .data(vis.data, function(d) {return d.key})
-                .attr("x1", function(d,i) {return i*(vis.chart.setup.bar_width+vis.chart.setup.bar_padding)+Math.floor((vis.chart.setup.bar_width)/2)})
-                .attr("x2", function(d,i) {return i*(vis.chart.setup.bar_width+vis.chart.setup.bar_padding)+Math.floor((vis.chart.setup.bar_width)/2)})
-                .attr("y1", function(d) {return vis.y_scale(d.value.low)})
-                .attr("y2", function(d) {return vis.y_scale(d.value.high)})
-                .classed("fall",function(d) {return d.value.open > d.value.close});
-            wick.enter().append("line")
-                .attr("class", "wick")
-                .attr("x1", function(d,i) {return i*(vis.chart.setup.bar_width+vis.chart.setup.bar_padding)+Math.floor((vis.chart.setup.bar_width)/2)})
-                .attr("x2", function(d,i) {return i*(vis.chart.setup.bar_width+vis.chart.setup.bar_padding)+Math.floor((vis.chart.setup.bar_width)/2)})
-                .attr("y1", function(d) {return vis.y_scale(d.value.low)})
-                .attr("y2", function(d) {return vis.y_scale(d.value.high)})
-                .style("stroke-dasharray", options.dasharray || undefined)
-                .classed("fall",function(d) {return d.value.open > d.value.close})
-                .on("mousemove", function() {vis.updateCursor()});
+                .attr('x1', function(d, i) {return i * (vis.chart.setup.bar_width + vis.chart.setup.bar_padding) + Math.floor((vis.chart.setup.bar_width) / 2) + (options.wickoffset * vis.chart.setup.bar_width || 0)})
+                .attr('x2', function(d, i) {return i * (vis.chart.setup.bar_width + vis.chart.setup.bar_padding) + Math.floor((vis.chart.setup.bar_width) / 2) + (options.wickoffset * vis.chart.setup.bar_width || 0)})
+                .attr('y1', function(d) {return vis.y_scale(d.value.low)})
+                .attr('y2', function(d) {return vis.y_scale(d.value.high)})
+                .classed('fall',function(d) {return d.value.open > d.value.close});
+            wick.enter().append('line')
+                .attr('class', 'wick')
+                .attr('x1', function(d, i) {return i * (vis.chart.setup.bar_width + vis.chart.setup.bar_padding) + Math.floor((vis.chart.setup.bar_width) / 2) + (options.wickoffset * vis.chart.setup.bar_width || 0)})
+                .attr('x2', function(d, i) {return i * (vis.chart.setup.bar_width + vis.chart.setup.bar_padding) + Math.floor((vis.chart.setup.bar_width) / 2) + (options.wickoffset * vis.chart.setup.bar_width || 0)})
+                .attr('y1', function(d) {return vis.y_scale(d.value.low)})
+                .attr('y2', function(d) {return vis.y_scale(d.value.high)})
+                .style('stroke-dasharray', options.dasharray || undefined)
+                .classed('fall',function(d) {return d.value.open > d.value.close})
+                .on('mousemove', function() {vis.updateCursor()});
             wick.exit().remove();
 
-            var candle = this.candles.selectAll("rect.cndl")
+            var candle = this.candles.selectAll('rect.cndl')
               .data(vis.data, function(d) {return d.key})
-                .attr("x", function(d,i) {return i*(vis.chart.setup.bar_width+vis.chart.setup.bar_padding)})
-                .attr("y", function(d) {return vis.y_scale(Math.max(d.value.open,d.value.close))})
-                .attr("height", function(d) {return Math.max(0.1,Math.abs(vis.y_scale(d.value.open)-vis.y_scale(d.value.close)))})
-                .classed("fall",function(d) {return d.value.open > d.value.close});
-            candle.enter().append("rect")
-                .attr("class", "cndl")
-                .attr("x", function(d,i) {return i*(vis.chart.setup.bar_width+vis.chart.setup.bar_padding)})
-                .attr("y", function(d) {return vis.y_scale(Math.max(d.value.open,d.value.close))})
-                .attr("width", function(d) {return vis.chart.setup.bar_width})
-                .attr("height", function(d) {return Math.max(0.1,Math.abs(vis.y_scale(d.value.open)-vis.y_scale(d.value.close)))})
-                .style("stroke-dasharray", options.dasharray || undefined)
-                .style("fill-opacity", options.fillopacity || undefined)
-                .classed("fall",function(d) {return d.value.open > d.value.close})
-                .on("mousemove", function() {vis.updateCursor()});
+                .attr('x', function(d, i) {return i * (vis.chart.setup.bar_width + vis.chart.setup.bar_padding)})
+                .attr('y', function(d) {return vis.y_scale(Math.max(d.value.open, d.value.close))})
+                .attr('height', function(d) {return Math.max(0.1, Math.abs(vis.y_scale(d.value.open) - vis.y_scale(d.value.close)))})
+                .classed('fall',function(d) {return d.value.open > d.value.close});
+            candle.enter().append('rect')
+                .attr('class', 'cndl')
+                .attr('x', function(d, i) {return i * (vis.chart.setup.bar_width + vis.chart.setup.bar_padding)})
+                .attr('y', function(d) {return vis.y_scale(Math.max(d.value.open, d.value.close))})
+                .attr('width', function(d) {return vis.chart.setup.bar_width})
+                .attr('height', function(d) {return Math.max(0.1, Math.abs(vis.y_scale(d.value.open) - vis.y_scale(d.value.close)))})
+                .style('stroke-dasharray', options.dasharray || undefined)
+                .style('fill-opacity', options.fillopacity || undefined)
+                .classed('fall',function(d) {return d.value.open > d.value.close})
+                .on('mousemove', function() {vis.updateCursor()});
             candle.exit().remove();
 
         }
