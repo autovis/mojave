f2895936f5aa2bbbc73005f8c1766dce33c641bb
autovis <autovis@users.noreply.github.com>
2016-02-15 08:16:30 -0500

    Remove 'zmq' dependency currently breaking in Heroku build

--- zmq_test.js
+++
@@ -1,9 +0,0 @@
-var zmq = require('zmq')
-var socket = zmq.socket('sub');
-
-socket.connect('tcp://127.0.0.1:2027');
-socket.subscribe('');
-
-socket.on('message', function(msg) {
-  console.log(msg.toString());
-});
