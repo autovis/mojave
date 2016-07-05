8bcfb4583bab61c87bb34a3303f259f6bd8691e6
autovis <autovis@users.noreply.github.com>
2016-07-05 17:30:06 -0400

    Initial changes to support FSMs and indicator macros

---
+++ common/indicators/_/FiniteStateMachine.js
@@ -0,0 +1,34 @@
+'use strict';
+
+define(['lodash', 'expression'], function(_, Expression) {
+
+    return {
+
+        param_names: ['state_config'],
+
+        input: ['_*'],
+        output: [
+                    ['state', 'string'],
+                    ['vars', 'object']
+        ],
+
+        initialize: function() {
+            if (this.param.output_type) this.output.set_type(this.param.output_type);
+            var expr_vars = _.defaults(this.param.vars, {
+                type: this.output.type,
+                unitsize: _.isObject(this.inputs[0].instrument) ? this.inputs[0].instrument.unit_size : null,
+                ticksize: _.isObject(this.inputs[0].instrument) ? this.inputs[0].instrument.tick_size : null
+            });
+            this.expr = new Expression(this.param.expr_string, {
+                vars: expr_vars,
+                streams: this.inputs
+            });
+            if (this.expr === null) throw new Error('Invalid expression: ' + this.param.expression);
+        },
+
+        on_bar_update: function() {
+            this.output.set(this.expr.evaluate());
+        }
+    };
+
+});
